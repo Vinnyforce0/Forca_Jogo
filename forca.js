843ddc69e6103dcf23a576054f3841af3218f15f
Vinnyforce0 <jorgevinny2008@gmail.com>
2025-06-06 23:12:18 -0300

    Update forca.js

--- forca.js
+++ forca.js
@@ -49,7 +49,7 @@ function processarEntrada() {
   if (entrada.startsWith('s ')) {
     const [_, posStr, letra] = entrada.split(' ')
     const pos = parseInt(posStr) - 1
-    if (pos >= 0 && pos < mascara.length && letra.lenght == 1) {
+    if (pos >= 0 && pos < mascara.length) {
       mascara[pos] = letra
       possibilidadesPorPosicao[pos] = new Set([letra])
     }
